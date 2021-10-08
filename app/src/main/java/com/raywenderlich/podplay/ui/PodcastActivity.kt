1eb07e11f008b86b3eb0e5afe4f8bde581f80543
Luna <lunarotimas@gmail.com>
2021-10-08 03:41:38 -0400

    pg480

--- app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -1,8 +1,12 @@
 package com.raywenderlich.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.raywenderlich.podplay.R
 import com.raywenderlich.podplay.repository.ItunesRepo
 import com.raywenderlich.podplay.service.ItunesService
@@ -23,4 +27,21 @@ class PodcastActivity : AppCompatActivity() {
             Log.i(TAG, "Results = ${results.body()}")
             }
     }
-}
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // 1
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+        // 2
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+        // 3
+        val searchManager = getSystemService(Context.SEARCH_SERVICE)
+                as SearchManager
+        // 4
+
+        searchView.setSearchableInfo(
+            searchManager.getSearchableInfo(componentName)
+        )
+        return true
+    }
+    }
