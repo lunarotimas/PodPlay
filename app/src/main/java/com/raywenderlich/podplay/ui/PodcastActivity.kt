bf194ee3c189c925d1835a7e3cc7151f429b4cfb
Luna <lunarotimas@gmail.com>
2021-10-08 08:02:27 -0400

    pg495

--- app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -7,27 +7,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.raywenderlich.podplay.R
+import com.raywenderlich.podplay.adapter.PodcastListAdapter
 import com.raywenderlich.podplay.databinding.ActivityPodcastBinding
 import com.raywenderlich.podplay.repository.ItunesRepo
 
+
 import com.raywenderlich.podplay.service.ItunesService
+import com.raywenderlich.podplay.viewmodel.SearchViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class PodcastActivity : AppCompatActivity() {
+class PodcastActivity : AppCompatActivity(),
+    PodcastListAdapter.PodcastListAdapterListener {
     val TAG = javaClass.simpleName
-    private lateinit var binding: ActivityPodcastBinding
+    private lateinit var databinding: ActivityPodcastBinding
+    private val searchViewModel by viewModels<SearchViewModel>()
+    private lateinit var podcastListAdapter: PodcastListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPodcastBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        databinding = ActivityPodcastBinding.inflate(layoutInflater)
+        setContentView(databinding.root)
 
         setupToolbar()
+        setupViewModels()
+        updateControls()
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // 1
@@ -47,14 +59,6 @@ class PodcastActivity : AppCompatActivity() {
         return true
     }
 
-    private fun performSearch(term: String) {
-        val itunesService = ItunesService.instance
-        val itunesRepo = ItunesRepo.ItunesRepo(itunesService)
-        GlobalScope.launch {
-            val results = itunesRepo.searchByTerm(term)
-            Log.i(TAG, "Results = ${results.body()}")
-        }
-    }
 
     private fun handleIntent(intent: Intent) {
         if (Intent.ACTION_SEARCH == intent.action) {
@@ -71,6 +75,50 @@ class PodcastActivity : AppCompatActivity() {
     }
 
     private fun setupToolbar() {
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(databinding.toolbar)
+    }
+
+    private fun setupViewModels() {
+        val service = ItunesService.instance
+        searchViewModel.iTunesRepo = ItunesRepo.ItunesRepo(service)
+    }
+
+    private fun updateControls() {
+        databinding.podcastRecyclerView.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(this)
+        databinding.podcastRecyclerView.layoutManager =
+            layoutManager
+        val dividerItemDecoration = DividerItemDecoration(
+            databinding.podcastRecyclerView.context,
+            layoutManager.orientation)
+
+        databinding.podcastRecyclerView.addItemDecoration(dividerItemDecoration)
+        podcastListAdapter = PodcastListAdapter(null, this, this)
+        databinding.podcastRecyclerView.adapter = podcastListAdapter
+    }
+
+    private fun showProgressBar() {
+        databinding.progressBar.visibility = View.VISIBLE
+    }
+    private fun hideProgressBar() {
+        databinding.progressBar.visibility = View.INVISIBLE
+    }
+
+    private fun performSearch(term: String) {
+        showProgressBar()
+        GlobalScope.launch {
+            val results = searchViewModel.searchPodcasts(term)
+            withContext(Dispatchers.Main) {
+                hideProgressBar()
+                databinding.toolbar.title = term
+                podcastListAdapter.setSearchData(results)
+            }
+        }
+    }
+
+    override fun onShowDetails(
+        podcastSummaryViewData: SearchViewModel.PodcastSummaryViewData
+    ) {
+        // Not implemented yet
     }
     }
