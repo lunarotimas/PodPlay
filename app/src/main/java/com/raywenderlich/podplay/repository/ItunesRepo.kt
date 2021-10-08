01ade981b8701b692d40a3cbb396a447f8e6eb52
Luna <lunarotimas@gmail.com>
2021-10-08 02:12:33 -0400

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/podplay/repository/ItunesRepo.kt
@@ -0,0 +1,12 @@
+package com.raywenderlich.podplay.repository
+
+import com.raywenderlich.podplay.service.ItunesService
+
+class ItunesRepo {
+    // 1
+    class ItunesRepo(private val itunesService: ItunesService) {
+        // 2
+        suspend fun searchByTerm(term: String) =
+            itunesService.searchPodcastByTerm(term) // 3
+    }
+}
---
+++ app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -0,0 +1,26 @@
+package com.raywenderlich.podplay.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.raywenderlich.podplay.R
+import com.raywenderlich.podplay.repository.ItunesRepo
+import com.raywenderlich.podplay.service.ItunesService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class PodcastActivity : AppCompatActivity() {
+    val TAG = javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_podcast)
+
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo.ItunesRepo(itunesService)
+        GlobalScope.launch {
+            val results = itunesRepo.searchByTerm("Android Developer")
+            Log.i(TAG, "Results = ${results.body()}")
+            }
+    }
+}
